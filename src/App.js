2610f8f9ae1e3d434a9ecdb242939c6d341fc3c0
JLSC24 <jlsc24dg@gmail.com>
2021-08-25 07:42:39 -0500

    Creación auth facebook

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
 import awsconfig from "./aws-exports";
 
-import { Button, Col, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 Amplify.configure(awsconfig);
 
@@ -20,11 +20,86 @@ const AuthStateApp = () => {
   const [user, setUser] = React.useState();
 
   React.useEffect(() => {
+    if (!window.FB) createScript();
+
     return onAuthUIStateChange((nextAuthState, authData) => {
       setAuthState(nextAuthState);
       setUser(authData);
     });
   }, []);
+
+  const signIn = () => {
+    const fb = window.FB;
+    fb.getLoginStatus((response) => {
+      if (response.status === "connected") {
+        getAWSCredentials(response.authResponse);
+      } else {
+        fb.login(
+          (response) => {
+            if (!response || !response.authResponse) {
+              return;
+            }
+            getAWSCredentials(response.authResponse);
+          },
+          {
+            // the authorized scopes
+            scope: "public_profile,email",
+          }
+        );
+      }
+    });
+  };
+
+  const getAWSCredentials = (response) => {
+    const { accessToken, expiresIn } = response;
+    const date = new Date();
+    const expires_at = expiresIn * 1000 + date.getTime();
+    if (!accessToken) {
+      return;
+    }
+
+    const fb = window.FB;
+    fb.api("/me", { fields: "name,email" }, (response) => {
+      const user = {
+        name: response.name,
+        email: response.email,
+      };
+
+      Auth.federatedSignIn(
+        "facebook",
+        { token: accessToken, expires_at },
+        user
+      ).then((credentials) => {
+        console.log(credentials);
+      });
+    });
+  };
+
+  const createScript = () => {
+    // load the sdk
+    window.fbAsyncInit = fbAsyncInit;
+    const script = document.createElement("script");
+    script.src = "https://connect.facebook.net/en_US/sdk.js";
+    script.async = true;
+    script.onload = initFB;
+    document.body.appendChild(script);
+  };
+
+  const initFB = () => {
+    const fb = window.FB;
+    console.log("FB SDK initialized");
+  };
+
+  const fbAsyncInit = () => {
+    // init the fb sdk client
+    const fb = window.FB;
+    fb.init({
+      appId: "541409930434877",
+      cookie: true,
+      xfbml: true,
+      version: "v2.11",
+    });
+  };
 
   return authState === AuthState.SignedIn && user ? (
     <div className="App">
@@ -41,9 +116,7 @@ const AuthStateApp = () => {
               <AmplifyGoogleButton
                 onClick={() => Auth.federatedSignIn({ provider: "Google" })}
               />
-              <AmplifyFacebookButton
-                onClick={() => Auth.federatedSignIn({ provider: "Facebook" })}
-              />
+              <AmplifyFacebookButton onClick={signIn} />
               <hr />
             </div>
           </AmplifySignIn>
