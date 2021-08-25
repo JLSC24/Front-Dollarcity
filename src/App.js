1b6861e0aeb31221996d4d3aa3872b9ea46b929f
JLSC24 <jlsc24dg@gmail.com>
2021-08-25 07:12:33 -0500

    Add authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Amplify, Auth } from "aws-amplify";
+import {
+  AmplifyAuthenticator,
+  AmplifySignOut,
+  AmplifySignIn,
+  AmplifyFacebookButton,
+  AmplifyGoogleButton,
+} from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
 
-function App() {
-  return (
+import { Button, Col, Row } from "react-bootstrap";
+
+Amplify.configure(awsconfig);
+
+const AuthStateApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Hello, {user.username} <AmplifySignOut />
+      </div>
     </div>
+  ) : (
+    <>
+      <Row className="mb-3">
+        <AmplifyAuthenticator>
+          <AmplifySignIn headerText="My Custom Sign In Text" slot="sign-in">
+            <div slot="federated-buttons">
+              <AmplifyGoogleButton
+                onClick={() => Auth.federatedSignIn({ provider: "Google" })}
+              />
+              <AmplifyFacebookButton
+                onClick={() => Auth.federatedSignIn({ provider: "Facebook" })}
+              />
+              <hr />
+            </div>
+          </AmplifySignIn>
+        </AmplifyAuthenticator>
+      </Row>
+    </>
   );
-}
+};
 
-export default App;
+export default AuthStateApp;
