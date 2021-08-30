439b62aac1d665c2161cdf094f4cab56ae9bc19b
JLSC24 <jlsc24dg@gmail.com>
2021-08-30 18:33:37 -0500

    corrección google auth

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const AuthStateApp = () => {
   const [user, setUser] = React.useState();
 
   React.useEffect(() => {
-    /* if (!window.FB) createScript();
+    if (!window.FB) createScript();
 
     const ga =
       window.gapi && window.gapi.auth2
@@ -41,7 +41,7 @@ const AuthStateApp = () => {
     return onAuthUIStateChange((nextAuthState, authData) => {
       setAuthState(nextAuthState);
       setUser(authData);
-    }); */
+    });
   }, []);
 
   const signInGoogle = () => {
@@ -180,7 +180,7 @@ const AuthStateApp = () => {
   ) : (
     <>
       <Row className="mb-3">
-        {/* <AmplifyAuthenticator>
+        <AmplifyAuthenticator>
           <AmplifySignIn headerText="Dollarcity" slot="sign-in">
             <div slot="federated-buttons">
               <AmplifyGoogleButton onClick={signInGoogle} />
@@ -188,8 +188,8 @@ const AuthStateApp = () => {
               <hr />
             </div>
           </AmplifySignIn>
-        </AmplifyAuthenticator> */}
-        <AmplifyAuthenticator federated={federated}></AmplifyAuthenticator>
+        </AmplifyAuthenticator>
+        {/* <AmplifyAuthenticator federated={federated}></AmplifyAuthenticator> */}
       </Row>
     </>
   );
